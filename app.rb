1158c352c6e3ef1f982723b8ae4e002b986fb373
Connor McGraw <42752783+Conmcgr@users.noreply.github.com>
2025-01-24 05:39:40 +0000

    logic down

--- app.rb
+++ app.rb
@@ -2,8 +2,59 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:home)
+end
+
+get("/rock") do
+  opp = rand(1..3)
+  @result = ""
+  @them = ""
+  @you = "rock"
+  if opp == 1
+    @result = "won"
+    @them = "scissors"
+  elsif opp == 2
+    @result = "lost"
+    @them = "paper"
+  else
+    @result = "tied"
+    @them = "rock"
+  end
+  erb(:play)
+end
+
+get("/paper") do
+  opp = rand(1..3)
+  @result = ""
+  @them = ""
+  @you = "paper"
+  if opp == 1
+    @result = "won"
+    @them = "rock"
+  elsif opp == 2
+    @result = "lost"
+    @them = "scissors"
+  else
+    @result = "tied"
+    @them = "paper"
+  end
+  erb(:play)
+end
+
+get("/scissors") do
+  opp = rand(1..3)
+  @result = ""
+  @them = ""
+  @you = "scissors"
+  if opp == 1
+    @result = "won"
+    @them = "paper"
+  elsif opp == 2
+    @result = "lost"
+    @them = "rock"
+  else
+    @result = "tied"
+    @them = "scissors"
+  end
+  erb(:play)
 end
